7d0e70c25d3056250614ad65c873cce582a12588
roryoung <rorymyoung@gmail.com>
2021-12-03 10:11:46 +0000

    Set up fixed noise experiments

---
+++ src/manifests/fixed_action_noise=0/walker_walk.py
@@ -0,0 +1,81 @@
+
+import realworldrl_suite.environments as rwrl
+
+from utils import merge_dict
+from callbacks import LoggerCallback, SaveModelCallback
+from manifests.common import get_all_consistent_agents, get_random_agent, get_ppo_agent, merge_manifest_and_agent
+
+
+def get_trial_manifest(noise):
+    trial = {
+        "trial_name": "flip_prob=" + str(noise).replace(".", ","),
+        "env_args": {
+            "noise_spec" : dict(fixed=dict(enable=bool(True*noise), actions_prob=noise, actions_value=0))
+        }
+    }
+    
+    return trial
+
+def get_base_manifest():
+    base_manifest = {
+        "exp_name": "fixed_action_noise=0/walker_walk",
+        "training_mode": "skip_existing",
+        
+        # Environment args
+        "env_class": rwrl.load,
+        "env_args": {
+            "domain_name": "walker",
+            "task_name": "realworld_walk",
+            "environment_kwargs": dict(log_safety_vars=False, flat_observation=True),
+        },
+        "bridge_args": {
+            "n_envs": 4
+        },
+
+        # Training args
+        "n_seeds": 3,
+        "learn": {
+            "total_timesteps": 1000 * 15000,
+            "callback_fns": [
+                {
+                    "callback": LoggerCallback,
+                    "args": {}
+                },
+                {
+                    "callback": SaveModelCallback,
+                    "args": {
+                        "backup_frequency": 1000 * 500
+                    }
+                }
+            ]
+        },
+
+        # Evaluation args
+        "eval_seed": {},
+        "eval_trial": {}
+    }
+
+    return base_manifest
+
+
+def get_agent_manifests():
+    all_agents = []
+    all_agents.append(get_ppo_agent())
+    all_agents.append(get_random_agent())
+    # all_agents += get_all_consistent_agents()
+
+    return all_agents
+
+
+def get_manifest():
+    noise_levels = [i/5 for i in range(6)]
+    # noise_levels = [0]
+    # noise_levels = []
+
+    # Get trail manifests
+    manifest = [merge_dict(get_base_manifest(), get_trial_manifest(noise)) for noise in noise_levels]
+
+    manifest = merge_manifest_and_agent(manifest, get_agent_manifests())
+    
+    return manifest
+
