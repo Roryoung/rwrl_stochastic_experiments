f509b6870ca3c6573f59e3181b352c6e2c9a6fdb
roryoung <rorymyoung@gmail.com>
2021-12-17 16:51:55 +0000

    Add gaussian joint damping experiments

---
+++ src/manifests/gaussian_joint_damping/walker_stand.py
@@ -0,0 +1,83 @@
+
+import realworldrl_suite.environments as rwrl
+from stable_baselines3 import PPO
+
+from utils import merge_dict
+from callbacks import LoggerCallback, SaveModelCallback
+from manifests.common import get_random_agent, get_ppo_agent, merge_manifest_and_agent
+
+
+def get_trial_manifest(noise):
+    trial = {
+        "trial_name": "std=" + str(noise).replace(".", ","),
+        "env_args": {
+            "perturb_spec": {
+                "enable": True,
+                'period': 1, 
+                'param': "joint_damping",
+                'scheduler': "random_walk", 
+                'std': noise
+            }
+        }
+    }
+    
+    return trial
+
+
+def get_base_manifest():
+    base_manifest = {
+        "exp_name": "gaussian_joint_damping/walker_stand",
+        "training_mode": "skip_existing",
+        "env_class": rwrl.load,
+        "env_args": {
+            "domain_name": "walker",
+            "task_name": "realworld_stand",
+            "environment_kwargs": dict(log_safety_vars=False, flat_observation=True),
+        },
+        "bridge_args": {
+            "n_envs": 8
+        },
+
+        "n_seeds": 3,
+        "learn": {
+            "total_timesteps": 1000 * 10000,
+            "callback_fns": [
+                {
+                    "callback": LoggerCallback,
+                    "args": {}
+                },
+                {
+                    "callback": SaveModelCallback,
+                    "args": {
+                        "backup_frequency": 1000 * 500
+                    }
+                }
+            ]
+        },
+        "eval_seed": {},
+        "eval_trial": {}
+    }
+
+    return base_manifest
+
+
+def get_agent_manifests():
+    all_agents = []
+    all_agents.append(get_ppo_agent())
+    all_agents.append(get_random_agent())
+    # all_agents += get_all_consistent_agents()
+    return all_agents
+
+
+def get_manifest():
+    noise_levels = [0,0.2,0.4,0.6,0.8,1]
+    # noise_levels = [0]
+    # noise_levels = []
+
+    # Get trail manifests
+    manifest = [merge_dict(get_base_manifest(), get_trial_manifest(noise)) for noise in noise_levels]
+
+    manifest = merge_manifest_and_agent(manifest, get_agent_manifests())
+    
+    return manifest
+
